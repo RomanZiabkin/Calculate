2b4b46e2c6a61ef18e6fbc810eb148002eba62f8
RomanZiabkin <romanziabkin@yandex.ru>
2022-11-14 21:50:16 +0300

    second commit

--- Calculate/ViewController.swift
+++ Calculate/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var operand = " "
+    var numberOne = " "
+    var numberTwo = " "
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    /* Функция нажатия кнопок 0...9
+     При нажатии на цифры, значения будут передаваться в переменную numberOne и отображаться в элементе resultLabel. Далее если будет нажата кнопка с операндом то значения будут записываться в переменную operand, и пользователь может ввести второе число, которое в свою очередь будет записываться в переменную numberTwo.
+     */
+    @IBAction func inputNumber(_ sender: UIButton) {
+        
+        if operand.isEmpty {
+            numberOne = numberOne + (sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        } else {
+            numberTwo = numberTwo + (sender.titleLabel?.text)!
+            resultLabel.text = numberTwo
+        }
+        
     }
-
-
+    
+    //Функция обнуления
+    @IBAction func clearAction(_ sender: UIButton) {
+        numberOne = " "
+        numberTwo = " "
+        operand = " "
+        resultLabel.text = "0"
+    }
+    
+    // Функция обработки нажатия на клавиши +, -, /, *
+    @IBAction func inputOperand(_ sender: UIButton) {
+        operand = (sender.titleLabel?.text! ?? <#default value#>)!
+    }
+    
+    //Кнопка результата
+    @IBAction func resultAction(_ sender: Any) {
+        var result = 0.0
+        
+        switch operand {
+        case "/":
+            result = Double(numberOne)! / Double(numberTwo)!
+        case "*":
+            result = Double(numberOne)! * Double(numberTwo)!
+        case "-":
+            result = Double(numberOne)! - Double(numberTwo)!
+        case "+":
+            result = Double(numberOne)! + Double(numberTwo)!
+        default:
+            break
+        }
+        
+        if result.truncatingRemainder(dividingBy: 1.0) == 00 {
+            resultLabel.text = String(Int(result))
+        } else {
+            resultLabel.text = String(result)
+        }
+    }
+    
 }
-
